0ebac34075f7e1458501457ba534e6d1aaf403f5
Aitor <blancomiguez@gmail.com>
2026-06-24 10:29:00 +0200

    add setup

---
+++ setup.R
@@ -0,0 +1,4 @@
+library(curatedMetagenomicData)
+curatedMetagenomicData("YachidaS_2019.relative_abundance",dryrun=F)
+curatedMetagenomicData("ZellerG_2016.relative_abundance",dryrun=F)
+curatedMetagenomicData("RaymondF_2014.pathway_abundance",dryrun=F)
